9dfb9a8f57075041a666c79a9a40bba5a33ead46
pablovass <faktory.fm@gmail.com>
2022-08-13 17:45:34 -0300

    ejecucion con dos implementacion una con beans

---
+++ src/main/java/com/pablovass/fundamentos/bean/MyBeanTwoImplement.java
@@ -0,0 +1,8 @@
+package com.pablovass.fundamentos.bean;
+
+public class MyBeanTwoImplement implements MyBean {
+    @Override
+   public void  print(){
+        System.out.println("hola desde my bean implementado");
+    }
+}
