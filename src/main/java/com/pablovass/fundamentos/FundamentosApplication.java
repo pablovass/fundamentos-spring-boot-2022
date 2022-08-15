d9494fcac8ee94a2b867b3de53dc2e9cf55667e5
pablovass <faktory.fm@gmail.com>
2022-08-15 10:28:59 -0300

    consultas jpql

--- src/main/java/com/pablovass/fundamentos/FundamentosApplication.java
+++ src/main/java/com/pablovass/fundamentos/FundamentosApplication.java
@@ -9,14 +9,13 @@ import com.pablovass.fundamentos.pojo.UserPojo;
 import com.pablovass.fundamentos.repository.UserRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -58,18 +57,34 @@ public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDepen
 	public void run(String... args)  {
 		//ejemplosAteriores();
 		saveUserInDataBase();
+		getInformationJpqlFromUser();
+}
+	private void  getInformationJpqlFromUser(){
+	//userRepository.findByUserEmail("roman@gmail.com");
+	LOGGER.info("usuario con el findByUserEmail "+
+			userRepository.findByUserEmail("Test4@domain.com")
+			.orElseThrow(()->new RuntimeException("No se encontro el usuario")));
+
+	userRepository.findByAndSort("user", Sort.by("id").descending())
+			.stream()
+			.forEach(user->LOGGER.info("usuario con metod sort"+ user));
 	}
 
 	private void  saveUserInDataBase(){
-		User user1= new User("John","John@gmail.com", LocalDate.of(2021,03,20));
-		User user2= new User("Miguel","miguel@gmail.com", LocalDate.of(2020,02,20));
-		User user3= new User("Jo","Jo@gmail.com", LocalDate.of(2019,01,20));
-		User user4= new User("Maria","maria@gmail.com", LocalDate.of(2019,01,20));
-		User user5= new User("Jose","Jose@gmail.com", LocalDate.of(2019,01,20));
-		User user6= new User("roman","roman@gmail.com", LocalDate.of(2019,01,20));
-
-		List<User>list= Arrays.asList(user1,user2,user3,user4,user5,user6);
-
+		User user1 = new User("John", "john@domain.com", LocalDate.of(2021, 03, 15));
+		User user2 = new User("Julie", "julie@domain.com", LocalDate.of(2021, 03, 20));
+		User user3 = new User("Daniela", "daniela@domain.com", LocalDate.of(2021, 03, 25));
+		User user4 = new User("Oscar", "oscar@domain.com", LocalDate.now());
+		User user5 = new User("Test1", "Test1@domain.com", LocalDate.now());
+		User user6 = new User("Test2", "Test2@domain.com", LocalDate.now());
+		User user7 = new User("Test3", "Test3@domain.com", LocalDate.now());
+		User user8 = new User("Test4", "Test4@domain.com", LocalDate.now());
+		User user9 = new User("Test5", "Test5@domain.com", LocalDate.now());
+		User user10 = new User("Test6", "Test6@domain.com", LocalDate.now());
+		User user11 = new User("Test7", "Test7@domain.com", LocalDate.now());
+		User user12 = new User("Test8", "Test8@domain.com", LocalDate.now());
+		User user13 = new User("Test9", "Test9@domain.com", LocalDate.now());
+		List<User> list = Arrays.asList(user4, user1, user3, user2, user5, user6, user7, user8, user9, user10, user11, user12, user13);
 		list.stream().forEach(userRepository::save);
 	}
 	private  void ejemplosAteriores(){
--- src/main/java/com/pablovass/fundamentos/repository/UserRepository.java
+++ src/main/java/com/pablovass/fundamentos/repository/UserRepository.java
@@ -1,10 +1,26 @@
 package com.pablovass.fundamentos.repository;
-
 import com.pablovass.fundamentos.entity.User;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
+
+    // este area siempre esta bacia siempre y cuando no tengamos que usar jpql o utilizar o sobrescribir los metodos de las clases estendidas
+    @Query("Select u from User u WHERE u.email=?1")
+    Optional<User>findByUserEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.name LIKE ?1%") //VAMOS A ORDENAR POR ORDEN
+    List<User> findByAndSort(String name, Sort sort);
+
+    //@Query("select u from User u where u.name like ?1%")
+    //List<User> findByAndSort(String name, Sort sort);
 }
